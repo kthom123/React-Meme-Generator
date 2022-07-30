086f348cd06a69b514ba93a32f40f74246788770
Kyle Thomas <kylejwthomas@gmail.com>
2022-07-30 17:52:17 +0900

    added meme data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,21 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    let url
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return (
         <main>
-            <form className="form">
+            <p>{url}</p>
+            <div className="form">
                 <input
                     type="text"
                     placeholder="Top text"
@@ -16,10 +28,11 @@ export default function Meme() {
                 />
                 <button
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
